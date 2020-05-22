4f206c8b33eb0af2699a4083d258200029c57557
atusy <30277794+atusy@users.noreply.github.com>
2020-05-22 21:29:31 +0900

    wip

---
+++ R/rich-html-document.R
@@ -0,0 +1,24 @@
+#' rmarkdown::html_documentの機能全部有効版
+#'
+#' `toc`, `toc_float`, `numbber_sections`, `code_folding`, `code_download`を
+#' 有効にした。
+#'
+#' @inheritParams rmarkdown::html_document
+#' @param ... `rmarkdown::html_document`に渡す引数。
+#'
+#' @export
+rich_html_document <- function(
+  # 後から無効にする可能性もあるなら、html_documentに渡す引数は
+  # bodyに直接書かず、引数として書いておくと良い
+  toc = TRUE,
+  toc_float = TRUE,
+  number_sections = TRUE,
+  code_folding = "show",
+  code_download = TRUE,
+  ...
+) {
+  rmarkdown::html_document(
+    toc = toc, toc_float = toc_float, number_sections = number_sections,
+    code_folding = code_folding, code_download = code_download
+  )
+}
---
+++ R/styled-html-document.R
@@ -0,0 +1,56 @@
+#' rmarkdown::html_documentにオリジナルスタイルシートを適用した版
+#'
+#' @inheritParams rmarkdown::html_document
+#' @param line_breaks
+#'   Rmd内の改行の扱い。
+#'   既定値は改行の前後が全角文字なら詰め、それ以外なら半角スペースを入れる
+#'   （`"east_asian_line_breaks"`）。
+#' @param ...
+#'   `rmarkdown::html_document`に渡す引数。
+#'
+#' @export
+styled_html_document <- function(
+  extra_dependencies = NULL,
+  md_extensions = NULL,
+  line_breaks = c(
+    "east_asian_line_breaks",
+    "hard_line_breaks",
+    "ignore_line_breaks",
+    "default"
+  ),
+  ...
+) {
+  line_breaks = match.arg(line_breaks)
+
+  # カスタマイズしたhtml_documentの生成
+  rmarkdown::html_document(
+    # ユーザーが指定した依存ファイルの方が優先されるように、
+    # dependencies, extra_dependenciesの順で指定する
+    extra_dependencies =
+      append(list(dep_css()), extra_dependencies),
+    # 同様にユーザーが指定したmd_extensionsを優先する
+    md_extensions =
+      c(if (line_breaks == "default") "" else line_breaks, md_extensions),
+    ...
+  )
+}
+
+
+
+
+
+#' styled_html_documentの依存ファイル
+#'
+#' @noRd
+dep_css <- function() {
+  # パッケージ名
+  pkg <- "tokyor85down"
+
+  htmltools::htmlDependency(
+    name = pkg, # 適当な名前。今回はパッケージ名にした。
+    version = packageVersion(pkg), # バージョン。今回はパッケージに準じた。
+    src = "css", # 依存ファイルの保存先。package引数を指定しておくと、inst以下のディレクトリを相対パスで指定できる。
+    stylesheet = "style.css", # 依存したいスタイルシートのファイル名
+    package = pkg # 依存ファイルを提供するパッケージの名前。
+  )
+}
